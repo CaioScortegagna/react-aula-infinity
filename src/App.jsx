78285c98c5803119437a35a3dd6c7c5c16de49aa
CaioScortegagna <129218941+CaioScortegagna@users.noreply.github.com>
2023-11-07 15:33:20 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import fp from './assets/foto.png'
+
+
+function App() {
+
+    
+  return(
+    <>
+      <h1>aula 1</h1>
+      <ul>
+        <li><img src={fp} alt=""/></li>
+        <h1></h1>
+      </ul>
+      
+    </>
+  )
+}
+
+export default App
